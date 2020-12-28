61ad23f72462b8d70aa5279c4eb8c28714b78735
Azizul Tareq <azizul.tareq@northsouth.edu>
2020-12-29 00:40:44 +0600

    text size and color styled

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,22 +8,29 @@ const Item = ({ data }) => {
             <div key={d.country}>
                 <div className="ui card">
   <div className="content">
-    <div className="header" style={{color: 'blue'}}>{d.country}</div>
+    <div className="header" style={{color: '#C41C77'}}>{d.country}</div>
   </div>
+ 
   <div className="content">
-    <h4 className="ui sub header">Total Cases:   <span className="second-word-formatting">{d.cases.total ? d.cases.total : '0'}</span></h4>
     <div className="ui small feed">
-      <div className="event">
+    <div className="event">
         <div className="content">
-          <div className="summary">
-          Total Deaths:   <span className="second-word-formatting">{d.deaths.total ? d.deaths.total : '0'}</span>
+          <div className="ui header" style={{color : '#D91B56'}}>
+          Total Cases:   <span className="second-word-formatting" style={{fontSize: '15px', color: '#D91B56'}}>{d.cases.total ? d.cases.total : '0'}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
-          <div className="summary">
-          Recovered:   <span className="second-word-formatting">{d.cases.recovered ? d.cases.recovered : '0'}</span>
+          <div className="ui header" style={{color : '#14289F'}}>
+          Total Deaths:   <span className="second-word-formatting" style={{fontSize: '15px', color: '#14289F'}}>{d.deaths.total ? d.deaths.total : '0'}</span>
+          </div>
+        </div>
+      </div>
+      <div className="event">
+        <div className="content">
+          <div className="ui header" style={{color: '#2CD91B'}}>
+          Recovered:   <span className="second-word-formatting" style={{fontSize: '15px', color: '#2CD91B'}}>{d.cases.recovered ? d.cases.recovered : '0'}</span>
           </div>
         </div>
       </div>
