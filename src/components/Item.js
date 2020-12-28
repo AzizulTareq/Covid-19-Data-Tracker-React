ebc0bbfd3a8347f35840f62c6e164d4c7dbcb8c6
Azizul Tareq <azizul.tareq@northsouth.edu>
2020-12-28 21:21:03 +0600

    components styled

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,47 +4,51 @@ import './item.css'
 const Item = ({ data }) => {
     
     const renderItem = data.map((d) => {
+
+     if(d.country != 'All' && d.country != 'Europe')
+    {
+
         return (
             <div key={d.country}>
                 <div className="ui card">
-  <div className="content every-card">
-    <div className="header" style={{color: 'red'}}>{d.country}</div>
+  <div className="content">
+    <div className="header" style={{color: 'blue'}}>{d.country}</div>
   </div>
   <div className="content">
-    <h4 className="ui sub header">Total Cases:   <span className="second-word-formatting">{d.cases.total}</span></h4>
+    <h4 className="ui sub header">Total Cases:   <span className="second-word-formatting">{d.cases.total ? d.cases.total : '0'}</span></h4>
     <div className="ui small feed">
       <div className="event">
         <div className="content">
           <div className="summary">
-          Total Deaths:   <span className="second-word-formatting">{d.deaths.total}</span>
+          Total Deaths:   <span className="second-word-formatting">{d.deaths.total ? d.deaths.total : '0'}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
           <div className="summary">
-          Recovered:   <span className="second-word-formatting">{d.cases.recovered}</span>
+          Recovered:   <span className="second-word-formatting">{d.cases.recovered ? d.cases.recovered : '0'}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
           <div className="summary">
-          Critical:   <span className="second-word-formatting">{d.cases.critical}</span>
+          Critical:   <span className="second-word-formatting">{d.cases.critical? d.cases.critical : '0'}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
           <div className="summary">
-          New Cases:   <span className="second-word-formatting">{d.cases.new}</span>
+          New Cases:   <span className="second-word-formatting">{d.cases.new? d.cases.new : '0'}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
           <div className="summary">
-          New Deaths:   <span className="second-word-formatting">{d.deaths.new == '' ? '0' : d.deaths.new}</span>
+          New Deaths:   <span className="second-word-formatting">{d.deaths.new? d.deaths.new : '0'}</span>
           </div>
         </div>
       </div>
@@ -55,6 +59,7 @@ const Item = ({ data }) => {
 </div>
             
         )
+    }
     })
 
     return <div className="grid">{renderItem}</div>
