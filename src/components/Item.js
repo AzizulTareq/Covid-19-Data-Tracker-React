e4bdde758e67247552404c08beba91c248f2125e
Azizul Tareq <azizul.tareq@northsouth.edu>
2020-12-28 19:36:58 +0600

    all information are showing

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,29 +8,43 @@ const Item = ({ data }) => {
             <div key={d.country}>
                 <div className="ui card">
   <div className="content every-card">
-    <div className="header">{d.country}</div>
+    <div className="header" style={{color: 'red'}}>{d.country}</div>
   </div>
   <div className="content">
-    <h4 className="ui sub header">Activity</h4>
+    <h4 className="ui sub header">Total Cases:   <span className="second-word-formatting">{d.cases.total}</span></h4>
     <div className="ui small feed">
       <div className="event">
         <div className="content">
           <div className="summary">
-            xyz
+          Total Deaths:   <span className="second-word-formatting">{d.deaths.total}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
           <div className="summary">
-            critical
+          Recovered:   <span className="second-word-formatting">{d.cases.recovered}</span>
           </div>
         </div>
       </div>
       <div className="event">
         <div className="content">
           <div className="summary">
-             new cases
+          Critical:   <span className="second-word-formatting">{d.cases.critical}</span>
+          </div>
+        </div>
+      </div>
+      <div className="event">
+        <div className="content">
+          <div className="summary">
+          New Cases:   <span className="second-word-formatting">{d.cases.new}</span>
+          </div>
+        </div>
+      </div>
+      <div className="event">
+        <div className="content">
+          <div className="summary">
+          New Deaths:   <span className="second-word-formatting">{d.deaths.new == '' ? '0' : d.deaths.new}</span>
           </div>
         </div>
       </div>
@@ -38,8 +52,7 @@ const Item = ({ data }) => {
   </div>
   
 </div>
-
-            </div>
+</div>
             
         )
     })
